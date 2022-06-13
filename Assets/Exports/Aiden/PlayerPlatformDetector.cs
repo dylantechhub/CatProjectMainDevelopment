b1babf759f36ea20727d3ad05d5a42fe8f459c25
Aiden C. Desjarlais <aiden.desjarlais2013@gmail.com>
2022-06-13 14:42:55 -0500

    Things

--- Assets/Exports/Aiden/PlayerPlatformDetector.cs
+++ Assets/Exports/Aiden/PlayerPlatformDetector.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerPlatformDetector : MonoBehaviour
 {
     private bool off;
+    public GameObject Cat;
     //detect if the player is standing on a platform type
     //this script is global to all platforms, with no hassle. Attach to the playerarmature.
     private void OnControllerColliderHit(ControllerColliderHit collision)
@@ -12,14 +13,14 @@ public class PlayerPlatformDetector : MonoBehaviour
         //GLOBAL PLATFORMS
             if (collision.gameObject.tag == "Platform")
             {
-            transform.SetParent(collision.transform);
+            Cat.transform.SetParent(collision.transform);
             off = true;
         } else if (off) {
             //end player rotation
             off = false;
                 print(collision.gameObject.tag);
-                this.transform.SetParent(null);
-                this.transform.position += new Vector3(0.0f, 1.0f, 0.0f);
+            Cat.transform.SetParent(null);
+            Cat.transform.position += new Vector3(0.0f, 1.0f, 0.0f);
             }
         }
 }
