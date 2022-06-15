275ee103a741ec26189d854dba20be6250c349af
Aiden C. Desjarlais <aiden.desjarlais2013@gmail.com>
2022-06-15 14:00:10 -0500

    Updated platforms and fixed cat

--- Assets/Exports/Aiden/MovingPlatform.cs
+++ Assets/Exports/Aiden/MovingPlatform.cs
@@ -2,107 +2,220 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovingPlatform : MonoBehaviour
-{
-    private float Delay;
-    private float gpms;
-    private bool X;
-    private bool Y;
-    private bool Z;
+    public class MovingPlatform : MonoBehaviour
+    {
+    
+    /// <summary>
+    /// Delay for when the platform switches its directions (seconds)
+    /// </summary>
+    [HideInInspector]
+    public float Delay { get; set; }
+
+
+    /// <summary>
+    /// Local speed
+    /// </summary>
+    [HideInInspector]
+    public float speed { set; get; }
+
+
+
+
+    /// <summary>
+    /// Enable/get platform movement on the X axis
+    /// </summary>
+    [HideInInspector]
+    public bool X { get; set; }
+
+    /// <summary>
+    /// Enable/get platform movement on the Y axis
+    /// </summary>
+    [HideInInspector]
+    public bool Y { get; set; }
+
+    /// <summary>
+    /// Enable/get platform movement on the Z axis
+    /// </summary>
+    [HideInInspector]
+    public bool Z { get; set; }
+
+
+
     private bool stop;
 
-    private float posX;
-    private float posY;
-    private float posZ;
-
-    private static bool forward;
-
-    private static bool reverse;
-
-    private bool UsingTrigger;
-
-    private bool GlobalDebugMode;
-
-    [Space(10)]
-    [SerializeField]
-    public static string MovingPlatformID;
-    public void SetMPID(string ID) {
-        if (ID != "")
-            MovingPlatformID = ID;
-            else
-            Debug.LogError($"[MPC] ERROR! Moving platform named {this.gameObject.name} does not have a set ID!");
+    /// <summary>
+    /// Stops the platform in its path
+    /// </summary>
+    [HideInInspector]
+    public void Pause()
+    {
+        stop = true;
+    }
+    /// <summary>
+    /// Resumes the platforms movement path if stopped
+    /// </summary>
+    [HideInInspector]
+    public void Resume()
+    {
+        stop = false;
+    }
+    /// <summary>
+    /// true if the platform is paused
+    /// </summary>
+    [HideInInspector]
+    public bool isPaused()
+    {
+        return stop;
     }
 
-    private List<string> METHODS = new List<string>();
+    /// <summary>
+    /// Retrieves the platforms ID
+    /// </summary>
+    public string ID()
+    {
+        return MovingPlatformID;
+    }
+
+    public float posX;
+    public float posY;
+    public float posZ;
+
+    /// <summary>
+    /// Moves the platform in the forwards direction
+    /// </summary>
+    [HideInInspector]
+    public void GoForwards()
+    {
+        reverse = false;
+        forward = true;
+    }
+    /// <summary>
+    /// Moves the platform in reverse
+    /// </summary>
+    [HideInInspector]
+    public void GoReverse()
+    {
+        reverse = true;
+        forward = false;
+    }
+    /// <summary>
+    /// Returns a "forwards" or "reverse" string, depending on what direction the platforms moving in
+    /// </summary>
+    [HideInInspector]
+    public string MovingWhere()
+    {
+        var moving = "";
+        if (forward) moving = "forwards"; else if (reverse) moving = "reverse";
+        return moving;
+    }
+
+    private bool forward;
+    private bool reverse;
 
 
-    private bool Direction;
+    /// <summary>
+    /// Change the Collision Detecting mode for this platform
+    /// </summary>
+    [HideInInspector]
+    public bool UsingTrigger { get; set; }
+
+        private bool GlobalDebugMode;
+
+        [Space(10)]
+        public string MovingPlatformID;
+
+    public void SetMPID(string ID)
+        {
+            if (ID != "")
+                MovingPlatformID = ID;
+            else
+                Debug.LogError($"[MPC] ERROR! Moving platform named {this.gameObject.name} does not have a set ID!");
+        }
+
+    /// <summary>
+    /// Change/get the Axis Methods
+    /// </summary>
+    [HideInInspector]
+    public List<string> METHODS { get; set; } = new List<string>();
+
+
+    /// <summary>
+    /// Chnage the direction of the platform
+    /// </summary>
+    [HideInInspector]
+    public bool Direction { get; set; }
+
 
     //the PlatformController calls this function to enable the platform.
-    public void EnablePlatform(float d, float gpmsPC, string METHOD_X, string METHOD_Y, string METHOD_Z, bool DIRECTION, bool usingTrigger, bool GDM)
-    {
-        GlobalDebugMode = GDM;
-        UsingTrigger = usingTrigger;
-        Direction = DIRECTION;
+    /// <summary>
+    /// manually enable a platform from another script. read docs for more information.
+    /// </summary>
+    [HideInInspector]
+    public void EnablePlatform(float delay, float speedFPC, string METHOD_X, string METHOD_Y, string METHOD_Z, bool DIRECTION, bool usingTrigger, bool globalDebugMode)
+        {
+            GlobalDebugMode = globalDebugMode;
+            UsingTrigger = usingTrigger;
+            Direction = DIRECTION;
             METHODS.Add(METHOD_X);
             METHODS.Add(METHOD_Y);
             METHODS.Add(METHOD_Z);
 
-        if (GlobalDebugMode) {
-            print("[MPC]: Global-Debug mode enabled!");
-            print($"[MPC]: Platform {MovingPlatformID} Enabled/Updated! Using Methods: "+METHOD_Z+ METHOD_Y+ METHOD_X+$" TriggerMode?: "+usingTrigger); 
-        }
-        
-        if(METHODS.Contains("X"))
-        {
-            X = true;
-        } 
-        if (METHODS.Contains("Y"))
-        {
-            Y = true;
-        } 
-        if (METHODS.Contains("Z"))
-        {
-            Z = true;
-        }
-        Delay = d;
-        gpms = gpmsPC;
-        forward = true;
-    }
+            if (GlobalDebugMode)
+            {
+                print("[MPC]: Global-Debug mode enabled!");
+                print($"[MPC]: Platform {MovingPlatformID} Enabled/Updated! Using Methods: " + METHOD_Z + METHOD_Y + METHOD_X + $" TriggerMode?: " + usingTrigger);
+            }
 
-    private void Awake()
-    {
-        posX = transform.position.x;
-        posY = transform.position.y;
-        posZ = transform.position.z;
-    }
+            if (METHODS.Contains("X"))
+            {
+                X = true;
+            }
+            if (METHODS.Contains("Y"))
+            {
+                Y = true;
+            }
+            if (METHODS.Contains("Z"))
+            {
+                Z = true;
+            }
+            Delay = delay;
+            speed = speedFPC;
 
+        //start! \/
+            forward = true;
+        }
+
+        private void Start()
+        {
+            posX = transform.position.x;
+            posY = transform.position.y;
+            posZ = transform.position.z;
+        print(posX+" "+posY+" "+posZ);
+        }
     private bool Align = true;
-
-    private void Update()
-    {
-
-
-        if (Align)
+        private void FixedUpdate()
+        {
+            //Alingners, keep the platform on path.
+            if (Align)
             if (X && !Y && !Z)
             {
-                transform.position = new Vector3(transform.position.x, posY, posZ);
+            transform.position = new Vector3(transform.position.x, posY, posZ);
             }
             else if (!X && Y && !Z)
             {
-                transform.position = new Vector3(posX, transform.position.y, posZ);
+            transform.position = new Vector3(posX, transform.position.y, posZ);
             }
             else if (!X && !Y && Z)
             {
-                transform.position = new Vector3(posX, posY, transform.position.z);
+            transform.position = new Vector3(posX, posY, transform.position.z);
             }
             else if (X && Y && !Z)
             {
-                transform.position = new Vector3(transform.position.x, transform.position.y, posZ);
+            transform.position = new Vector3(transform.position.x, transform.position.y, posZ);
             }
             else if (!X && Y && Z)
             {
-                transform.position = new Vector3(posX, transform.position.y, transform.position.z);
+            transform.position = new Vector3(posX, transform.position.y, transform.position.z);
             }
             else if (X && Y && Z)
             {
@@ -111,174 +224,176 @@ public class MovingPlatform : MonoBehaviour
             }
 
 
-        //FORWARD DIRECTION AXIS CONTROLLER
 
-        //X Axis
-        if (forward && X && !Y && !Z && !Direction)
-            transform.Translate(Vector3.right * gpms);
-        else if (reverse && X && !Y && !Z && !Direction)
-            transform.Translate(Vector3.left * gpms);
+            //FORWARD DIRECTION AXIS CONTROLLER
 
-        //Y Axis
-        if (forward && Y && !X && !Z && !Direction)
-            transform.Translate(Vector3.up * gpms);
-        else if (reverse && Y && !X && !Z && !Direction)
-            transform.Translate(Vector3.down * gpms);
+            //X Axis
+            if (forward && X && !Y && !Z && !Direction)
+                transform.Translate(Vector3.right * speed);
+            else if (reverse && X && !Y && !Z && !Direction)
+                transform.Translate(Vector3.left * speed);
 
-        //Z Axis
-        if (forward && Z && !Y && !X && !Direction)
-            transform.Translate(Vector3.forward * gpms);
-        else if (reverse && Z && !Y && !X && !Direction)
-            transform.Translate(Vector3.back * gpms);
+            //Y Axis
+            if (forward && Y && !X && !Z && !Direction)
+                transform.Translate(Vector3.up * speed);
+            else if (reverse && Y && !X && !Z && !Direction)
+                transform.Translate(Vector3.down * speed);
 
-
-
-
-
-
-        //MULTI-AXIS-CONTROLLERS
-
-        //X + Y Axis
-        if (forward && Y && X && !Direction)
-            transform.Translate(new Vector3(1, 1, 0) * gpms);
-        else if (reverse && Y && X && !Direction)
-            transform.Translate(new Vector3(-1, -1, 0) * gpms);
-
-        //Z + Y Axis
-        if (forward && Z && Y && !Direction)
-            transform.Translate(new Vector3(0, 1, 1) * gpms);
-        else if (reverse && Z && Y && !Direction)
-            transform.Translate(new Vector3(0, -1, -1) * gpms);
-
-
-
-
-        //REVERSE DIRECTION AXIS CONTROLLER
-
-        //X Axis
-        if (forward && X && !Y && !Z && Direction)
-            transform.Translate(Vector3.left * gpms);
-        else if (reverse && X && !Y && !Z && Direction)
-            transform.Translate(Vector3.right * gpms);
-
-        //Y Axis
-        if (forward && Y && !X && !Z && Direction)
-            transform.Translate(Vector3.down * gpms);
-        else if (reverse && Y && !X && !Z && Direction)
-            transform.Translate(Vector3.up * gpms);
-
-        //Z Axis
-        if (forward && Z && !Y && !X && Direction)
-            transform.Translate(Vector3.back * gpms);
-        else if (reverse && Z && !Y && !X && Direction)
-            transform.Translate(Vector3.forward * gpms);
+            //Z Axis
+            if (forward && Z && !Y && !X && !Direction)
+                transform.Translate(Vector3.forward * speed);
+            else if (reverse && Z && !Y && !X && !Direction)
+                transform.Translate(Vector3.back * speed);
 
 
 
 
 
 
-        //MULTI-AXIS-CONTROLLERS
+            //MULTI-AXIS-CONTROLLERS
 
-        //X + Y Axis
-        if (forward && Y && X && Direction)
-            transform.Translate(new Vector3(-1, 1, 0) * gpms);
-        else if (reverse && Y && X && Direction)
-            transform.Translate(new Vector3(1, -1, 0) * gpms);
+            //X + Y Axis
+            if (forward && Y && X && !Direction)
+                transform.Translate(new Vector3(1, 1, 0) * speed);
+            else if (reverse && Y && X && !Direction)
+                transform.Translate(new Vector3(-1, -1, 0) * speed);
 
-        //Z + Y Axis
-        if (forward && Z && Y && Direction)
-            transform.Translate(new Vector3(0, 1, -1) * gpms);
-        else if (reverse && Z && Y && Direction)
-            transform.Translate(new Vector3(0, -1, 1) * gpms);
+            //Z + Y Axis
+            if (forward && Z && Y && !Direction)
+                transform.Translate(new Vector3(0, 1, 1) * speed);
+            else if (reverse && Z && Y && !Direction)
+                transform.Translate(new Vector3(0, -1, -1) * speed);
 
-        if (stop)
-        {
-            forward = false;
-            reverse = false;
-        }
-    }
 
-    void OnTriggerEnter(Collider collision)
-    {
-        if (collision.gameObject.tag == MovingPlatformID+"_REVERSE" && UsingTrigger)
-        {
-            forward = false;
-            reverse = false;
-            if (GlobalDebugMode)
-            print($"[MPC]: {MovingPlatformID} Reverse");
-            StartCoroutine(Reverse());
-        }
-        else if (collision.gameObject.tag == MovingPlatformID + "_FORWARDS" && UsingTrigger)
-        {
-            forward = false;
-            reverse = false;
-            if (GlobalDebugMode)
-            print($"[MPC]: {MovingPlatformID} Forwards");
-            StartCoroutine(Forwards());
-        }
-        else if (!UsingTrigger)
-        {
-            if (GlobalDebugMode)
-            print($"[MPC]: {MovingPlatformID} Hit Trigger in Collider mode, Disregarding");
-        }
-        else
-        {
-            if (GlobalDebugMode)
-            Debug.LogWarning($"[MPC]: Warning! Platform ID: {MovingPlatformID} Hit GameObject Name {collision.gameObject.name} That is NOT a Platform Director!");
+
+
+            //REVERSE DIRECTION AXIS CONTROLLER
+
+            //X Axis
+            if (forward && X && !Y && !Z && Direction)
+                transform.Translate(Vector3.left * speed);
+            else if (reverse && X && !Y && !Z && Direction)
+                transform.Translate(Vector3.right * speed);
+
+            //Y Axis
+            if (forward && Y && !X && !Z && Direction)
+                transform.Translate(Vector3.down * speed);
+            else if (reverse && Y && !X && !Z && Direction)
+                transform.Translate(Vector3.up * speed);
+
+            //Z Axis
+            if (forward && Z && !Y && !X && Direction)
+                transform.Translate(Vector3.back * speed);
+            else if (reverse && Z && !Y && !X && Direction)
+                transform.Translate(Vector3.forward * speed);
+
+
+
+
+
+
+            //MULTI-AXIS-CONTROLLERS
+
+            //X + Y Axis
+            if (forward && Y && X && Direction)
+                transform.Translate(new Vector3(-1, 1, 0) * speed);
+            else if (reverse && Y && X && Direction)
+                transform.Translate(new Vector3(1, -1, 0) * speed);
+
+            //Z + Y Axis
+            if (forward && Z && Y && Direction)
+                transform.Translate(new Vector3(0, 1, -1) * speed);
+            else if (reverse && Z && Y && Direction)
+                transform.Translate(new Vector3(0, -1, 1) * speed);
+
+            if (stop)
+            {
+                forward = false;
+                reverse = false;
+            }
         }
 
-    }
+        void OnTriggerEnter(Collider collision)
+        { 
+        StartCoroutine(Reverse());
+            if (collision.gameObject.tag == MovingPlatformID + "_REVERSE" && UsingTrigger)
+            {
+                stop = true;
+                if (GlobalDebugMode)
+                    print($"[MPC]: {MovingPlatformID} Reverse");
+                StartCoroutine(Reverse());
+            }
+            else if (collision.gameObject.tag == MovingPlatformID + "_FORWARDS" && UsingTrigger)
+            {
+                stop = true;
+                if (GlobalDebugMode)
+                    print($"[MPC]: {MovingPlatformID} Forwards");
+                StartCoroutine(Forwards());
+            }
+            else if (!UsingTrigger)
+            {
+                if (GlobalDebugMode)
+                    print($"[MPC]: {MovingPlatformID} Hit Trigger in Collider mode, Disregarding");
+            }
+            else
+            {
+                if (GlobalDebugMode)
+                    Debug.LogWarning($"[MPC]: Warning! Platform ID: {MovingPlatformID} Hit GameObject Name {collision.gameObject.name} That is NOT a Platform Director!");
+            }
 
-    void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.tag == MovingPlatformID + "_REVERSE" && !UsingTrigger)
-        {
-            forward = false;
-            reverse = false;
-            if (GlobalDebugMode)
-            print($"[MPC]: {MovingPlatformID}: Hit Reverse");
-            StartCoroutine(Reverse());
-        }
-        else if (collision.gameObject.tag == MovingPlatformID + "_FORWARDS" && !UsingTrigger)
-        {
-            forward = false;
-            reverse = false;
-            if (GlobalDebugMode)
-            print($"[MPC]: {MovingPlatformID} Hit Forwards");
-            StartCoroutine(Forwards());
-        } else if (UsingTrigger)
-        {
-            if (GlobalDebugMode)
-            print($"[MPC]: {MovingPlatformID} Hit Collider in Trigger mode, Disregarding");
-        }
-        else
-        {
-            if (GlobalDebugMode)
-            Debug.LogWarning($"[MPC]: Warning! Platform ID: {MovingPlatformID} Hit GameObject name {collision.gameObject.name} This is NOT a Platform Director!");
         }
 
-    }
+        void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.tag == MovingPlatformID + "_REVERSE" && !UsingTrigger)
+            {
+                stop = true;
+                if (GlobalDebugMode)
+                    print($"[MPC]: {MovingPlatformID}: Hit Reverse");
+                StartCoroutine(Reverse());
+            }
+            else if (collision.gameObject.tag == MovingPlatformID + "_FORWARDS" && !UsingTrigger)
+            {
+                stop = true;
+                if (GlobalDebugMode)
+                    print($"[MPC]: {MovingPlatformID} Hit Forwards");
+                StartCoroutine(Forwards());
+            }
+            else if (UsingTrigger)
+            {
+                if (GlobalDebugMode)
+                    print($"[MPC]: {MovingPlatformID} Hit Collider in Trigger mode, Disregarding");
+            }
+            else
+            {
+                if (GlobalDebugMode)
+                    Debug.LogWarning($"[MPC]: Warning! Platform ID: {MovingPlatformID} Hit GameObject name {collision.gameObject.name} This is NOT a Platform Director!");
+            }
+
+        }
+
 
     private IEnumerator Forwards()
-    {
-        //adds delay
-        yield return new WaitForSeconds(Delay);
-        forward = true;
-        reverse = false;
-        stop = false;
-        if (GlobalDebugMode)
-        print("[MPC]: Forwards For ID: " + MovingPlatformID+ " (waited: " +Delay+"s)");
-    }
+        {
+            //adds delay
+            yield return new WaitForSeconds(Delay);
+            forward = true;
+            reverse = false;
+            stop = false;
+            if (GlobalDebugMode)
+                print("[MPC]: Forwards For ID: " + MovingPlatformID + " (waited: " + Delay + "s)");
+        }
+
+
     private IEnumerator Reverse()
-    {
-        //adds delay
-        yield return new WaitForSeconds(Delay);
-        forward = false;
-        reverse = true;
-        stop = false;
-        gameObject.transform.Translate(Vector3.back * gpms);
-        if (GlobalDebugMode)
-        print("[MPC]: Reverse For ID: " + MovingPlatformID + " (waited: " + Delay + "s)");
+        {
+            //adds delay
+            yield return new WaitForSeconds(Delay);
+            forward = false;
+            reverse = true;
+            stop = false;
+            gameObject.transform.Translate(Vector3.back * speed);
+            if (GlobalDebugMode)
+                print("[MPC]: Reverse For ID: " + MovingPlatformID + " (waited: " + Delay + "s)");
+        }
     }
-}
--- Assets/Exports/Aiden/PlayerPlatformDetector.cs
+++ Assets/Exports/Aiden/PlayerPlatformDetector.cs
@@ -11,33 +11,30 @@ public class PlayerPlatformDetector : MonoBehaviour
     private void OnControllerColliderHit(ControllerColliderHit collision)
     {
         //GLOBAL PLATFORMS
-            if (collision.gameObject.tag == "Platform")
-            {
+        if (collision.gameObject.tag == "Platform")
+        {
             Cat.transform.SetParent(collision.transform);
             off = true;
-        } else if (off) {
-            //end
-            off = false;
-            Cat.transform.SetParent(null);
-            Cat.transform.position += new Vector3(0.0f, 1.0f, 0.0f);
-            }
         }
+    }
 
     Ray ray;
 
     void Update()
     {
-        ray = new Ray(transform.position, -transform.up);
-        Debug.DrawRay(ray.origin, ray.direction * 5);
-        RaycastHit hitData;
-        if (Physics.Raycast(ray, out hitData, 5))
+        if (off)
         {
-            if (!hitData.transform.gameObject.GetComponent<MovingPlatform>())
+            ray = new Ray(transform.position, -transform.up);
+            Debug.DrawRay(ray.origin, ray.direction * 5);
+            RaycastHit hitData;
+            if (Physics.Raycast(ray, out hitData, 5))
             {
-                //end
-                off = false;
-                Cat.transform.SetParent(null);
-                Cat.transform.position += new Vector3(0.0f, 1.0f, 0.0f);
+                if (!hitData.transform.gameObject.GetComponent<MovingPlatform>())
+                {
+                    //end
+                    off = false;
+                    Cat.transform.SetParent(null);
+                }
             }
         }
     }
