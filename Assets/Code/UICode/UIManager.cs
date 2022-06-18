28097b35caf1903ec798600998f4e7c8c6915020
Aiden C. Desjarlais <71170613+TOG11@users.noreply.github.com>
2022-06-17 20:06:32 -0500

    MAJOR UI changes. Added some settings and fixed many things.
    Also, the controller can handle itself. no need to create an entire program to switch buttons.

--- Assets/Code/UICode/UIManager.cs
+++ Assets/Code/UICode/UIManager.cs
@@ -11,19 +11,24 @@ using StarterAssets;
 public class UIManager : MonoBehaviour
 {
     [Header("Options menu")]
-    public Slider VolumeSlider;
     public AudioMixer AudioMixer;
-    public float CurrentVolume;
+    public bool inOptions;
 
     [Header("UI stuff")]
-    public Image UImenu;
-    public Image OptionsMenu;
+    public GameObject UImenu;
+    public GameObject OptionsMenu;
 
     [Header("Controller UI")]
-    public Button[] CUIbuttons;
-    public MonoBehaviour[] CallFunction;
-    public int currentbutton;
-    
+    public List<Button> CUIbuttons = new List<Button>();
+    public List<Slider> CUISliders = new List<Slider>();
+    public int currentbutton = 0;
+    public int currentOption = 44444;
+    //lock controller controls for uses for other options
+    public bool ControllerLock;
+
+    //if the controller is controlling a slider of some sort.
+    public bool ControllerSliding;
+
 
     //Keybinding stuff
     private bool Escenabled = false;
@@ -32,79 +37,180 @@ public class UIManager : MonoBehaviour
     
     private void Update()
     {
-        if (ControllerMenu)
-        {
-            CUIbuttons[currentbutton].interactable = false;
-        }
         if (Keyboard.current.escapeKey.wasPressedThisFrame)
         {
-            UImenu.gameObject.SetActive(true);
-            GameObject.Find("cat").GetComponent<StarterAssetsInputs>().Menuopen = true;
+            UImenu.SetActive(true);
+            StarterAssets.StarterAssetsInputs.Menuopen = true;
             Escenabled = true;
         }
-        if (Gamepad.current != null && Gamepad.current.startButton.wasPressedThisFrame)
+
+        //controller menu open
+        if (Gamepad.current != null && Gamepad.current.startButton.wasPressedThisFrame && !ControllerLock)
         {
-            UImenu.gameObject.SetActive(true);
-            GameObject.Find("cat").GetComponent<StarterAssetsInputs>().Menuopen = true;
+            UImenu.SetActive(true);
+            //changed the variable Menuopen to static, allowing this. no refrence needs to be made to change this variable.
+            StarterAssets.StarterAssetsInputs.Menuopen = true;
             Escenabled = true;
             ControllerMenu = true;
         }
+
+        //if menu opened
         if (Escenabled)
         {
-            if (Gamepad.current != null && Gamepad.current.leftStick.up.wasPressedThisFrame)
-            {
+
+            //sort through menu up
+            if (Gamepad.current != null && Gamepad.current.leftStick.up.wasPressedThisFrame && !ControllerLock && !inOptions)
                 currentbutton--;
-                if (currentbutton < -0)
-                {
-                    currentbutton = 2;
-                }
-                
-            }
-            if (Gamepad.current != null && Gamepad.current.leftStick.down.wasPressedThisFrame)
-            {
+                if (currentbutton < 0)
+                    currentbutton = 3;
+
+            //sort through menu down
+            if (Gamepad.current != null && Gamepad.current.leftStick.down.wasPressedThisFrame && !ControllerLock && !inOptions)
                 currentbutton++;
-                if (currentbutton > 2)
-                {
+                if (currentbutton > 3)
                     currentbutton = 0;
-                }
-            }
-            if (Gamepad.current != null && Gamepad.current.aButton.wasPressedThisFrame)
+
+            //if controller went left to options menu, then allow changing of the options
+            if (Gamepad.current != null && Gamepad.current.leftStick.left.wasPressedThisFrame && !ControllerLock && !inOptions)
             {
-                CUIbuttons[currentbutton].onClick.Invoke();
+                currentbutton = 44444;
+                inOptions = true;
+                //options: 0: SFXVol, 1: MSTRVol, 2: MSICVol,
+                currentOption = 0;
+            }
+
+            //if controller went right to menu from options, then allow menu selection
+            if (Gamepad.current != null && Gamepad.current.leftStick.right.wasPressedThisFrame && !ControllerLock && inOptions)
+            {
+                //options: 0: continue, 1: restart, 2: options, 3: quit
+                currentbutton = 3;
+                inOptions = false;
+                currentOption = 44444;
+            }
+
+
+            //back button
+            if (Gamepad.current != null && Gamepad.current.bButton.wasPressedThisFrame)
+            {
+                //if controller is locked when b is pressed, unlock it. if pressed again close menu. if pressed within JUST the menu, it will close the menu.
+                if (ControllerLock)
+                {
+                    ControllerLock = false;
+                    currentbutton = 3;
+                    inOptions = false;
+                    currentOption = 44444;
+                }
+                else
+                    Continue();
+
+            }
+
+            //detect if slider is being changed, then go into slider changing mode
+            if (Gamepad.current != null && Gamepad.current.leftStick.left.wasPressedThisFrame && inOptions)
+            {
+                ControllerSliding = true;
+                ControllerLock = true;
+            } else
+            {
+                ControllerSliding = false;
+            }
+
+
+            if (!inOptions && !ControllerLock)
+            if (currentbutton==0)CUIbuttons[currentbutton].Select();if(currentbutton==1)CUIbuttons[currentbutton].Select();if(currentbutton==2)CUIbuttons[currentbutton].Select();if(currentbutton==3)CUIbuttons[currentbutton].Select();
+
+            if (inOptions)
+            {
+                //sort through options up
+                if (Gamepad.current != null && Gamepad.current.leftStick.up.wasPressedThisFrame && !ControllerLock)
+                    currentOption++;
+                    if (currentOption > 2)
+                        currentOption = 0;
+
+                //sort through options down
+                if (Gamepad.current != null && Gamepad.current.leftStick.down.wasPressedThisFrame && !ControllerLock)
+                    currentOption--;
+                if (currentOption < 0)
+                    currentOption = 2;
+
+
+
             }
         }
     }
 
-    public void LateUpdate()
+
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    
+
+
+    public void OpenOptions()
+    {
+            if(!OptionsMenu.activeSelf)
+            OptionsMenu.SetActive(true);
+            else
+            OptionsMenu.SetActive(false);
+    }
+
+    public void ChangeSFXVolume(Slider slider)
     {
         if (ControllerMenu)
         {
-            CUIbuttons[0].interactable = true;
-            CUIbuttons[1].interactable = true;
-            CUIbuttons[2].interactable = true;
+            slider.wholeNumbers = true;
+            AudioMixer.SetFloat("SFXVol", slider.value);
+            slider.wholeNumbers = false;
+        }    
+        else
+        {
+            AudioMixer.SetFloat("SFXVol", slider.value);
         }
-
     }
 
-    public void OnRestart()
+    public void ChangeMusicVolume(Slider slider)
     {
-        Application.LoadLevel(Application.loadedLevel);
+        if (ControllerMenu)
+        {
+            slider.wholeNumbers = true;
+            AudioMixer.SetFloat("MusicVol", slider.value);
+            slider.wholeNumbers = false;
+        }
+        else
+        {
+            AudioMixer.SetFloat("MusicVol", slider.value);
+        }
     }
 
-    public void OnPlay()
+    public void ChangeMasterVolume(Slider slider)
     {
-        
+        if (ControllerMenu)
+        {
+            slider.wholeNumbers = true;
+            AudioMixer.SetFloat("MasterVol", slider.value);
+            slider.wholeNumbers = false;
+        }
+        else
+        {
+            AudioMixer.SetFloat("MasterVol", slider.value);
+        }
     }
 
-    public void OnContinue()
+
+    public void Continue()
     {
-        UImenu.gameObject.SetActive(false);
-        GameObject.Find("cat").GetComponent<StarterAssetsInputs>().Menuopen = false;
+        UImenu.SetActive(false);
+        OptionsMenu.SetActive(false);
+        inOptions = false;
+        ControllerLock = false;
+        StarterAssets.StarterAssetsInputs.Menuopen = false;
         Escenabled = false;
         ControllerMenu = false;
     }
 
-    public void OnApplicationQuit()
+    public void ApplicationQuit()
     {
         Application.Quit();
         Debug.Log("Quit");
--- Assets/PlayerStuff/ThirdPersonController/Scripts/ThirdPersonController.cs
+++ Assets/PlayerStuff/ThirdPersonController/Scripts/ThirdPersonController.cs
@@ -262,7 +262,7 @@ namespace StarterAssets
 
             // note: Vector2's != operator uses approximation so is not floating point error prone, and is cheaper than magnitude
             // if there is a move input rotate player when the player is moving
-            if (_input.move != Vector2.zero )
+            if (_input.move != Vector2.zero && !canclimb)
             {
                 _targetRotation = Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg + _mainCamera.transform.eulerAngles.y;
                 float rotation = Mathf.SmoothDampAngle(transform.eulerAngles.y, _targetRotation, ref _rotationVelocity, RotationSmoothTime);
